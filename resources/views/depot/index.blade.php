ba39d227207f0205b8fa0c41c71c9dfe4b2dac5d
TeyebNi <teyebnine30@gmail.com>
2026-07-26 11:48:56 +0000

    Fix unresolved merge conflict in depot/index.blade.php

--- resources/views/depot/index.blade.php
+++ resources/views/depot/index.blade.php
@@ -1,11 +1,6 @@
-
 @extends('layouts.master')
 
-
-
-
 @section('title')
-
 Dashboard Courier
 @endsection
 
@@ -26,74 +21,30 @@ Dashboard Courier
                                 <div class="table-responsive">
                                     <table class="table">
                                         <thead class=" text-primary">
-                                           <th>
-                                                N°
-                                            </th>
-                                            <th>
-                                                Code
-                                            </th>
-                                            <th>
-                                                Type de Demande
-                                            </th>
-                                            <th>
-                                                Nom
-                                            </th>
-                                            <th class="text-right">
-                                                NNI
-                                            </th>
-                                            <th class="text-right">
-                                                Tel
-                                            </th>
-                                             <th class="text-right">
-                                               Adresse
-                                            </th>
-                                            <th class="text-right">
-                                                Date
-                                            </th>
-                                            <th class="text-right">
-                                                Action
-                                            </th>
+                                           <th>N°</th>
+                                            <th>Code</th>
+                                            <th>Type de Demande</th>
+                                            <th>Nom</th>
+                                            <th class="text-right">NNI</th>
+                                            <th class="text-right">Tel</th>
+                                             <th class="text-right">Adresse</th>
+                                            <th class="text-right">Date</th>
+                                            <th class="text-right">Action</th>
                                         </thead>
                                         <tbody>
-<<<<<<< HEAD
-                                          @foreach($tabdepots as $key=>$tabdepot)
-=======
                                           @foreach($tabdepot as $key=>$item)
->>>>>>> 1817cc7f428a827ca79743958efb01e013ccb3d4
                                             <tr>
-                                                <td>
-                                                  {{++$key}}
-                                                </td>
-                                                <td>
-                                                  {{$item->id}}
-                                                </td>
-                                                <td>
-                                                   {{$item->typdm}}
-                                                </td>
-                                                <td class="text-right">
-                                                    {{$item->nom}}
-                                                </td>
-                                                  <td class="text-right">
-                                                   {{$item->nni}}
-                                                </td>
-                                                <td class="text-right">
-                                                    {{$item->tel}}
-                                                </td>
-                                                  <td class="text-right">
-                                                    {{$item->adresse}}
-                                                </td>
-                                                  <td class="text-right">
-                                                    {{$item->daterecp}}
-                                                </td>
-<<<<<<< HEAD
-                                                <td> <a href ="{{route('depot.print_reçu',$tabdepot->id)}}"  type="button"  class="btn btn-success"> Print</a>
-    <a  data-id="{{$tabdepot->id}}" data-typdm="{{$tabdepot->typdm}}" data-nom="{{$tabdepot->nom}}" data-nni="{{$tabdepot->nni}}" data-adresse="{{$tabdepot->adresse}}"  data-tel="{{$tabdepot->tel}}" data-daterecp="{{$tabdepot->daterecp}}" data-toggle="modal" data-target="#exampleModal-edit" type="button" class="btn btn-info"> Edit</a> 
- <a   data-id="{{$tabdepot->id}}" data-toggle="modal" data-target="#exampleModal-delete"  type="button" class="btn btn-danger"> Delete</a></td>
-=======
+                                                <td>{{++$key}}</td>
+                                                <td>{{$item->id}}</td>
+                                                <td>{{$item->typdm}}</td>
+                                                <td class="text-right">{{$item->nom}}</td>
+                                                  <td class="text-right">{{$item->nni}}</td>
+                                                <td class="text-right">{{$item->tel}}</td>
+                                                  <td class="text-right">{{$item->adresse}}</td>
+                                                  <td class="text-right">{{$item->daterecp}}</td>
                                                 <td><a  data-id="{{$item->id}}" data-typdm="{{$item->typdm}}" data-nom="{{$item->nom}}" data-nni="{{$item->nni}}" data-adresse="{{$item->adresse}}"  data-tel="{{$item->tel}}" data-daterecp="{{$item->daterecp}}"   data-toggle="modal" data-target="#exampleModal-show" type="button" class="btn btn-success"> Show</a>
     <a  data-id="{{$item->id}}" data-typdm="{{$item->typdm}}" data-nom="{{$item->nom}}" data-nni="{{$item->nni}}" data-adresse="{{$item->adresse}}"  data-tel="{{$item->tel}}" data-daterecp="{{$item->daterecp}}" data-toggle="modal" data-target="#exampleModal-edit" type="button" class="btn btn-info"> Edit</a> 
  <a   data-id="{{$item->id}}" data-toggle="modal" data-target="#exampleModal-delete"  type="button" class="btn btn-danger"> Delete</a></td>
->>>>>>> 1817cc7f428a827ca79743958efb01e013ccb3d4
                                             </tr>
                                             @endforeach
                                         </tbody>
@@ -101,17 +52,8 @@ Dashboard Courier
                                 </div>
                             </div>
                             <div class="d-flex justify-content-center mt-3">
-                    {{ $tabdepots->appends(request()->query())->links() }}
+                    {{ $tabdepot->appends(request()->query())->links() }}
                 </div>
-                        </div>
-                    </div>
-                    <div class="col-md-12">
-                        <div class="card card-plain">
-                            <div class="card-body">
-                                <div class="d-flex justify-content-center">
-                                    {{ $tabdepot->links() }}
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -120,8 +62,6 @@ Dashboard Courier
 
 
 @section('scripts')
-
-
 @endsection
 
 <!-- Modal -->
@@ -138,8 +78,6 @@ Dashboard Courier
 
          <form action="{{route('depot.store')}}" method="post">
         @csrf
-        
-        
       <div class="input-group">
         <div class="input-group-prepend">
         <span class="input-group-text">Type de Demande</span>
@@ -157,9 +95,7 @@ Dashboard Courier
         <span class="input-group-text">NNI</span>
       </div>
       <input type="text" class="form-control" name="nni" placeholder="Entrer NNI">
-      
                     <span  class="text-danger"></span>
-                   
     </div>
       <br>
       <div class="input-group">
@@ -167,21 +103,15 @@ Dashboard Courier
         <span class="input-group-text">Nom</span>
       </div>
       <input type="text" class="form-control" name="nom" placeholder="Entrer Nom">
-     
                     <span  class="text-danger"></span>
-                   
     </div>
       <br>
-      
-      
       <div class="input-group">
         <div class="input-group-prepend">
         <span class="input-group-text">Tel</span>
       </div>
       <input type="text" class="form-control" name="tel" placeholder="Entrer Tel">
-      
                     <span  class="text-danger"></span>
-                    
     </div>
      <br>
      <div class="input-group">
@@ -189,22 +119,16 @@ Dashboard Courier
         <span class="input-group-text">Adresse</span>
       </div>
       <input type="text" class="form-control" name="adresse" placeholder="Entrer Adresse">
-      
                     <span  class="text-danger"></span>
-                   
     </div>
-      
      <br>
    <div class="input-group">
         <div class="input-group-prepend">
         <span class="input-group-text">Date </span>
       </div>
       <input type="date" class="form-control" name="daterecp" placeholder="Entrer Date">
-     
                     <span  class="text-danger"></span>
-                   
     </div>
-    
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -214,7 +138,6 @@ Dashboard Courier
     </div>
   </div>
 </div>
- </div>
  <!-- Modal edit-->
 <div class="modal fade" id="exampleModal-edit" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog  modal-notify modal-lg modal-right modal-success" role="document">
@@ -235,9 +158,7 @@ Dashboard Courier
         <span class="input-group-text">Code</span>
       </div>
       <input type="text" class="form-control" name="PersonID" placeholder="Entrer Code">
-     
                     <span  class="text-danger"></span>
-                   
     </div>
     <br>
       <div class="input-group">
@@ -257,9 +178,7 @@ Dashboard Courier
         <span class="input-group-text">NNI</span>
       </div>
       <input type="text" class="form-control" name="nni" placeholder="Entrer NNI">
-      
                     <span  class="text-danger"></span>
-                   
     </div>
       <br>
       <div class="input-group">
@@ -267,21 +186,15 @@ Dashboard Courier
         <span class="input-group-text">Nom</span>
       </div>
       <input type="text" class="form-control" name="nom" placeholder="Entrer Nom">
-     
                     <span  class="text-danger"></span>
-                   
     </div>
       <br>
-      
-      
       <div class="input-group">
         <div class="input-group-prepend">
         <span class="input-group-text">Tel</span>
       </div>
       <input type="text" class="form-control" name="tel" placeholder="Entrer Tel">
-      
                     <span  class="text-danger"></span>
-                    
     </div>
      <br>
      <div class="input-group">
@@ -289,22 +202,16 @@ Dashboard Courier
         <span class="input-group-text">Adresse</span>
       </div>
       <input type="text" class="form-control" name="adresse" placeholder="Entrer Adresse">
-      
                     <span  class="text-danger"></span>
-                   
     </div>
-      
      <br>
    <div class="input-group">
         <div class="input-group-prepend">
         <span class="input-group-text">Date </span>
       </div>
       <input type="date" class="form-control" name="daterecp" placeholder="Entrer Date">
-     
                     <span  class="text-danger"></span>
-                   
     </div>
-    
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -314,29 +221,25 @@ Dashboard Courier
     </div>
   </div>
 </div>
- </div>
  <!-- Modal Show-->
 <div class="modal fade" id="exampleModal-show" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog  modal-notify modal-lg modal-right modal-success" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Affichage</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <button type="button" class="close" data-dismmiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
 
         <form action="" method="post">
-        
         <div class="input-group">
         <div class="input-group-prepend">
         <span class="input-group-text">Code</span>
       </div>
       <input type="text" class="form-control" name="PersonID" placeholder="Entrer Code">
-     
                     <span  class="text-danger"></span>
-                   
     </div>
     <br>
       <div class="input-group">
@@ -356,9 +259,7 @@ Dashboard Courier
         <span class="input-group-text">NNI</span>
       </div>
       <input type="text" class="form-control" name="nni" placeholder="Entrer NNI">
-      
                     <span  class="text-danger"></span>
-                   
     </div>
       <br>
       <div class="input-group">
@@ -366,21 +267,15 @@ Dashboard Courier
         <span class="input-group-text">Nom</span>
       </div>
       <input type="text" class="form-control" name="nom" placeholder="Entrer Nom">
-     
                     <span  class="text-danger"></span>
-                   
     </div>
       <br>
-      
-      
       <div class="input-group">
         <div class="input-group-prepend">
         <span class="input-group-text">Tel</span>
       </div>
       <input type="text" class="form-control" name="tel" placeholder="Entrer Tel">
-      
                     <span  class="text-danger"></span>
-                    
     </div>
      <br>
      <div class="input-group">
@@ -388,32 +283,24 @@ Dashboard Courier
         <span class="input-group-text">Adresse</span>
       </div>
       <input type="text" class="form-control" name="adresse" placeholder="Entrer Adresse">
-      
                     <span  class="text-danger"></span>
-                   
     </div>
-      
      <br>
    <div class="input-group">
         <div class="input-group-prepend">
         <span class="input-group-text">Date </span>
       </div>
       <input type="date" class="form-control" name="daterecp" placeholder="Entrer Date">
-     
                     <span  class="text-danger"></span>
-                   
     </div>
-    
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-       
       </div>
       </form>
     </div>
   </div>
 </div>
- </div>
 
 <!-- Modal Delete-->
 <div class="modal fade left " id="exampleModal-delete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -440,10 +327,8 @@ Dashboard Courier
        </div>
   </div>
 </div>
- </div>
 
  <script>
-
 $('#exampleModal-show').on('show.bs.modal',function(event){
   var button=$(event.relatedTarget)
    var typdm=button.data('typdm')
@@ -453,7 +338,6 @@ $('#exampleModal-show').on('show.bs.modal',function(event){
   var adresse=button.data('adresse')
   var daterecp=button.data('daterecp')
   var id= button.data('id')
-            
   var modal=$(this)
   modal.find('.modal-title').text('edit demande Information');
   modal.find('.modal-body #typdm').val(typdm);
@@ -463,7 +347,6 @@ $('#exampleModal-show').on('show.bs.modal',function(event){
   modal.find('.modal-body #adresse').val(adresse);
   modal.find('.modal-body #daterecp').val(daterecp);
   modal.find('.modal-body #id').val(id); 
-              
   });
   $('#exampleModal-edit').on('show.bs.modal',function(event){
   var button=$(event.relatedTarget)
@@ -474,7 +357,6 @@ $('#exampleModal-show').on('show.bs.modal',function(event){
   var adresse=button.data('adresse')
   var daterecp=button.data('daterecp')
   var id= button.data('id')
-            
   var modal=$(this)
   modal.find('.modal-title').text('edit demande Information');
   modal.find('.modal-body #typdm').val(typdm);
@@ -484,12 +366,10 @@ $('#exampleModal-show').on('show.bs.modal',function(event){
   modal.find('.modal-body #adresse').val(adresse);
   modal.find('.modal-body #daterecp').val(daterecp);
   modal.find('.modal-body #id').val(id); 
-              
   });
   $('#exampleModal-delete').on('show.bs.modal',function(event){
   var button=$(event.relatedTarget)
   var fournisseur_id= button.data('id')
-            
             var modal=$(this)
             modal.find('.modal-title').text('delete demande Information');
             modal.find('.modal-body #id').val(id);
